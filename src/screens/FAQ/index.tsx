4ade3563ac57cbaa28a25bb10fd0d74294bea494
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-06-28 19:39:09 -0300

    FAQ screen style done

--- src/screens/FAQ/index.tsx
+++ src/screens/FAQ/index.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import { Container } from './styles'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import {
+  CollapseBox,
+  CollapseStroke,
+  Container,
+  Information,
+  Title,
+  Wrapper,
+} from './styles'
 import { StatusBar } from 'expo-status-bar'
 import { HeaderHome } from '../../components/HeaderHome'
+import { CaretDown } from 'phosphor-react-native'
+import { useTheme } from 'styled-components/native'
+import Collapsible from 'react-native-collapsible'
 
 export function FAQ() {
+  const [collapse, setCollapse] = useState(true)
+  const theme = useTheme()
+
+  function handleCollapse() {
+    setCollapse(!collapse)
+  }
+
   return (
     <>
       <StatusBar translucent style="light" />
       <HeaderHome />
       <Container>
-        <Text>FAQ screen</Text>
+        <CollapseStroke>
+          <CollapseBox onPress={handleCollapse} expanded={!collapse}>
+            <Wrapper>
+              <Title expanded={!collapse}>Question</Title>
+              <CaretDown
+                size={32}
+                color={collapse ? theme.colors.gray800 : theme.colors.white}
+              />
+            </Wrapper>
+            <Collapsible collapsed={collapse}>
+              <Information expanded={!collapse}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit,
+                nisi et reprehenderit deleniti perspiciatis culpa esse inventore
+                nesciunt nam magni molestias, qui, iure ab accusantium tenetur
+                doloremque! Tempora, cumque enim.
+              </Information>
+            </Collapsible>
+          </CollapseBox>
+        </CollapseStroke>
       </Container>
     </>
   )
--- src/screens/FAQ/styles.ts
+++ src/screens/FAQ/styles.ts
@@ -1,5 +1,67 @@
-import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled, { css } from 'styled-components/native'
+
+interface CollapseProps {
+  expanded: boolean
+}
 
 export const Container = styled.View`
   flex: 1;
+
+  background-color: ${(props) => props.theme.colors.gray200};
+
+  padding: 20px;
+`
+
+export const CollapseStroke = styled.View`
+  border: 1px solid ${(props) => props.theme.colors.gray400};
+  border-radius: 5px;
+`
+
+export const CollapseBox = styled(RectButton)<CollapseProps>`
+  background-color: ${(props) => props.theme.colors.white};
+
+  border-radius: 5px;
+
+  padding: 8px 11px;
+
+  ${({ expanded }) =>
+    expanded &&
+    css`
+      background-color: ${(props) => props.theme.colors.blue600};
+    `}
+`
+
+export const Wrapper = styled.View`
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Title = styled.Text<CollapseProps>`
+  color: ${(props) => props.theme.colors.gray800};
+
+  font-size: ${RFValue(20)}px;
+  font-family: ${(props) => props.theme.fonts.regular};
+
+  ${({ expanded }) =>
+    expanded &&
+    css`
+      color: ${(props) => props.theme.colors.white};
+    `}
+`
+
+export const Information = styled.Text<CollapseProps>`
+  color: ${(props) => props.theme.colors.gray800};
+
+  font-size: ${RFValue(15)}px;
+  font-family: ${(props) => props.theme.fonts.regular};
+
+  ${({ expanded }) =>
+    expanded &&
+    css`
+      color: ${(props) => props.theme.colors.white};
+    `}
 `
