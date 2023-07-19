8ce6a6756e088aa8bbe03d87a6149b899d3824aa
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-07-19 19:52:10 -0300

    Route validation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { AppTabRoutes } from './app.tab.routes'
+import { useState } from 'react'
+import { Signin } from '../screens/Signin'
 
 export function Routes() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   return (
     <NavigationContainer>
-      <AppTabRoutes />
+      {!isLoggedIn ? <Signin /> : <AppTabRoutes />}
     </NavigationContainer>
   )
 }
