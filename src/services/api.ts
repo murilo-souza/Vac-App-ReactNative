16851bb5983dd3f8de3276b3712852f12effca7d
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-09-17 22:22:50 -0300

    API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://vaccine-71554-default-rtdb.firebaseio.com/',
+  baseURL:
+    'https://vaccine-71554-default-rtdb.firebaseio.com/UsersData/9ifemwpVrQSW9S6CYe6DCgcNIIf1/readings/',
 })
