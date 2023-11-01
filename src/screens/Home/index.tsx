83a5afadab1f233a38e2608c2c84f0f5f489d46c
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-11-01 15:02:22 -0300

    app

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,8 +4,8 @@ import { HeaderHome } from '../../components/HeaderHome'
 import { StatusBar } from 'expo-status-bar'
 import { VaccineCard } from '../../components/VaccineCard'
 import { useNavigation } from '@react-navigation/native'
-import { HumidityNotification } from '../../utils/humidityNotification'
-import { Button } from 'react-native'
+// import { HumidityNotification } from '../../utils/humidityNotification'
+// import { Button } from 'react-native'
 
 export function Home() {
   const navigation = useNavigation()
